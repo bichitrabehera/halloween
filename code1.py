2d7c6fb1473ba6cacf2d6f68fc43053ae589688f
bichitrabehera <bichitrabehera.amcec@gmail.com>
2025-10-17 19:03:26 +0530

    r1 done

--- code1.py
+++ code1.py
@@ -1,13 +1,30 @@
-letters = ["q","w","e","r","t","y","u","i","o","p",
-           "a","s","d","f","g","h","j","k","l","z",
-           "x","c","v","b","n","m","f","o","x","g",
-           "l","o","v","e"]
+letters = ['q','w','e','r','t','y','u','i','o','p',
+           'a','s','d','f','g','h','j','k','l','z',
+           'x','c','v','b','n','m']
 
-indices = [0b26 - 0b1,5+9-10,0b28 - 0b19,3*2 + 0b4 - 2,int("28",16)-24,0b30 - 0b22,int("1010",2) - 2,4*5 - 12 ]
+fog = [0b11, 0x7, 0o14]
+whispers = ("echo" * 2)[::-1]
+shadows = [0b101, 0x4, 11]
+
+i1 = (0b1100 + 0b1)
+i2 = (2 << 1) + 3 + 2
+i3 = sum([10 - 3, 0])
+i4 = (0x11 - 4)
+i5 = int(((3 << 3) - 7) / 2)
+i6 = (len("glow") + 13) - 10
+i7 = ord('x') - 100
+i8 = (int("2")**2) - 3 + 2
+
+indices = [12, 7, 19, 13, 17, 7, 21, 1]
 
 flower = ""
+for idx in indices:
+    flower += letters[idx + 1]
 
-for i in indices:
-    flower += letters[i]  # fix syntax errors
+for f in fog + shadows:
+    flower += "" * f
+
+for w in whispers:
+    flower += "" if w in "aeiou" else ""
 
 print(flower)
--- src/phaser/scenes/ForestScene.ts
+++ src/phaser/scenes/ForestScene.ts
@@ -67,32 +67,33 @@ letters = ['q','w','e','r','t','y','u','i','o','p',
            'a','s','d','f','g','h','j','k','l','z',
            'x','c','v','b','n','m']
 
-fog = [3, 7, 12]
-whispers = "echo" * 2
+fog = [0b11, 0x7, 0o14]
+whispers = ("echo" * 2)[::-1]
 shadows = [0b101, 0x4, 11]
 
-i1 = (0b1101 + 0b0*7)
-i2 = 2**3
-i3 = sum([2, 18])
-i4 = int(7*2)
-i5 = len("hello world") - 3
-i6 = 0x8
-i7 = ord('w') - ord('b') + 15
-i8 = int("3") + 1
+i1 = (0b1100 + 0b1)
+i2 = (2 << 1) + 3 + 2
+i3 = sum([10 - 3, 0])
+i4 = (0x11 - 4)
+i5 = int(((3 << 3) - 7) / 2)
+i6 = (len("glow") + 13) - 10
+i7 = ord('x') - 100
+i8 = (int("2")**2) - 3 + 2
 
-indices = [i1, i2, i3, i4, i5, i6, i7, 12]
+indices = [12, 7, 19, 13, 17, 7, 21, 1]
 
 flower = ""
 for idx in indices:
-    flower += letters[idx]
+    flower += letters[idx + 1]
 
 for f in fog + shadows:
-    flower += ""
+    flower += "" * f
 
 for w in whispers:
-    flower += ""
+    flower += "" if w in "aeiou" else ""
 
 print(flower)
+
 `;
 
     this.add
